5d69ff8a63204bb62d6ae28cde419ba5a8c8207f
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2024-06-08 16:32:35 +0600

    made it more mobile responsive

--- resources/views/partial/about.blade.php
+++ resources/views/partial/about.blade.php
@@ -12,15 +12,16 @@
                 <p class="mb-6">
                     As an industry leader, we follow international standards and best practices in all our projects. Our commitment to excellence is reflected in our adherence to regulations such as NFPA58, OHSAS18001, and ASME Section VIII, ensuring the safety and reliability of our solutions.
                 </p>
-                <ul class="list-disc mb-6 sm:mx-3">
-                    <li>Ministry of Power, Energy and Mineral Resources Bangladesh, LP Gas Operational Licensing Guidelines Gazette January 29, 2016.</li>
-                    <li>Bangladesh LP Gas Rules 2004</li>
-                    <li>Bangladesh LP Gas Rules 2017</li>
-                    <li>LP Gas pricing Mechanism developed by the Bangladesh Energy Regulatory Commission</li>
-                    <li>NFPA58 (Liquefied Petroleum Gas Code)</li>
-                    <li>OHSAS18001 (Occupational Health and Safety Management System)</li>
-                    <li>ASME Section VIII, Division 2 or EN 13445</li>
+                <ul class="list-disc mb-6 sm:mx-3 p-4 sm:p-6 bg-white dark:bg-gray-800 rounded-lg ">
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">Ministry of Power, Energy and Mineral Resources Bangladesh, LP Gas Operational Licensing Guidelines Gazette January 29, 2016.</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">Bangladesh LP Gas Rules 2004</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">Bangladesh LP Gas Rules 2017</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">LP Gas pricing Mechanism developed by the Bangladesh Energy Regulatory Commission</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">NFPA58 (Liquefied Petroleum Gas Code)</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">OHSAS18001 (Occupational Health and Safety Management System)</li>
+                    <li class="text-sm sm:text-base text-gray-700 dark:text-gray-300">ASME Section VIII, Division 2 or EN 13445</li>
                 </ul>
+                
                 <p>
                     Innotech Energy Engineering is committed to driving positive change through responsible and sustainable projects. Our integrated approach, combining engineering, fabrication, and operations, ensures seamless project delivery and customer satisfaction.
                 </p>
